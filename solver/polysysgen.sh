3c57d6fddb4251f77f751590df82a947ab67979f
rusydi <rusydi.hasan@gmail.com>
2017-10-30 17:48:26 +0100

    modification based on Marc's suggestion

---
+++ solver/polysysgen.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+CPPFILENAME="generator.cpp"
+DEG=2 #default degree is quadratic
+
+cleanup()
+{
+    if [ -f "a.out" ]; then
+        rm -f a.out
+    fi
+}
+
+display_help()
+{
+    echo "Random overdefined polyomial equations' generator over a finite field"
+    echo "Usage : $0 -f <fieldsize> -n <no. of variables> -m <no. of equations> [-d <degree>]"
+    echo "Arguments"
+    echo "  -h                    : display this help"
+    echo "  -f <fieldsize>        : set the size of the finite field"
+    echo "  -n <no. of variables> : set the number of variables"
+    echo "  -m <no. of equations> : set the number of equations"
+    echo "  -d <degree>           : (optional) set the degree (default=2)"
+    exit 0
+}
+
+cleanup
+while getopts :hf:n:m:d: option
+do
+    case "$option" in
+        h)
+            display_help
+            ;;
+        n)
+            N=$OPTARG
+            ;;
+        m)
+            M=$OPTARG
+            ;;
+        f)
+            FIELDSIZE=$OPTARG
+            ;;
+        d)
+            DEG=$OPTARG
+            ;;
+        \?)
+            echo "Invalid options: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument" >&2
+            exit 1;
+            ;;
+    esac
+done
+
+if [ -z "$FIELDSIZE" ]; then
+    echo "The field size is not set (use -f)"
+    exit 1
+fi
+if [ -z "$M" ]; then
+    echo "The number of equations is not set (use -m)"
+    exit 1
+fi
+if [ -z "$N" ]; then
+    echo "The number of variables is not set (use -n)"
+    exit 1
+fi
+
+re='^[0-9]+$'
+if ! [[ $FIELDSIZE =~ $re ]] || [ $FIELDSIZE -lt 2 ]; then
+    echo "The field size (-f) must be a positive integer >= 2"
+    exit 1
+fi
+if ! [[ $N =~ $re ]] || [ $N -lt 1 ]; then
+    echo "The number of variables (-n) must be a positive integer"
+    exit 1
+fi
+if ! [[ $M =~ $re ]] || [ $M -lt 1 ]; then
+    echo "The number of equations (-m) must be a positive integer"
+    exit 1
+fi
+if ! [[ $DEG =~ $re ]] || [ $DEG -lt 1 ]; then
+    echo "The degree (-d) must be an integer"
+    exit 1
+fi
+
+if [ $M -le $N ]; then
+    echo "the number of equations must be greater than the number of variables"
+    exit 1
+fi
+
+echo "size of the finite field : $FIELDSIZE"
+echo "number of variables      : $N"
+echo "number of equations      : $M"
+echo "degree                   : $DEG"
+
+pushd ../ && \
+    make FIELDSIZE=${FIELDSIZE} MAXVARS=${N} NPOLYS=${M} DEG=${DEG} generator &&\
+    popd
+
+pushd ../bin && \
+    ./generator_n${N}_m${M}_${FIELDSIZE} && \
+    popd
